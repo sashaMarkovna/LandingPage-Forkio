eaa0d5cad2f2de235029fabd2ea703b64dbb35a9
Alexandra <sashatrvl@gmail.com>
2018-09-04 16:40:54 +0300

    initial commit

--- src/js/partials/menu.js
+++ src/js/partials/menu.js
@@ -1,15 +1,9 @@
 $(document).ready(function(){
 
     $("#menu").on("click","a", function (event) {
-
         event.preventDefault();
-        //забираем идентификатор бока с атрибута href
-        var id  = $(this).attr('href'),
-
-        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        let id  = $(this).attr('href');
         top = $(id).offset().top;
-
-        //анимируем переход на расстояние - top за 1500 мс
         $('body,html').animate({scrollTop: top}, 1000);
     });
 });
